ad3458821b2736607d62e140859b13db4c2863cd
Rafael Sousa <rafaelsousa.ja.adv@gmail.com>
2019-07-03 11:56:09 -0300

    Criação das telas, incompleto.

---
+++ app/src/main/java/com/example/danilo/appdebts/MainWindow.java
@@ -0,0 +1,66 @@
+package com.example.danilo.appdebts;
+
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.danilo.appdebts.DAO.DebtsDAO;
+import com.example.danilo.appdebts.adapters.DebtsAdapter;
+import com.example.danilo.appdebts.database.DatabaseHelper;
+
+public class MainWindow extends AppCompatActivity {
+
+    RecyclerView mListDebts;
+    DebtsAdapter mDebtsAdapter;
+    DebtsDAO mDebtsDAO;
+    private SQLiteDatabase mConnection;
+    private DatabaseHelper mDataHelper;
+    private ConstraintLayout mLayout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_window);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mListDebts = findViewById(R.id.recycler_view_debts);
+        mLayout = findViewById(R.id. layout);
+        createConnection();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager( this);
+        mListDebts.setLayoutManager(linearLayoutManager);
+        mDebtsAdapter = new DebtsAdapter(mDebtsDAO.listDebts());
+        mListDebts.setAdapter(mDebtsAdapter);
+        mListDebts.setHasFixedSize(true);
+    }
+    private void createConnection(){
+        try{
+            mDataHelper = new DatabaseHelper(this);
+            mConnection = mDataHelper.getWritableDatabase();
+            mDebtsDAO = new DebtsDAO(mConnection);
+            Snackbar.make(mLayout, R.string.sucess_connection, Snackbar.LENGTH_LONG).show();
+        }catch (SQLException e){
+            Snackbar.make(mLayout, e.toString(), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+}
